a3fa37b3c619b96e00419c7fd48b2e68891cf799
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2021-03-23 23:04:54 +0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+def max_rot(n):
+    arr = list(str(n))
+    max1 = n
+    for i in range(len(arr)-1):
+        k = i
+        while (k < len(arr)-1):
+            temp = arr[k]
+            arr[k] = arr[k+1]
+            arr[k+1] = temp
+            k += 1
+        if (int(''.join(arr)) >= max1):
+            max1 = int(''.join(arr))
+    return max1
+print(max_rot(56789))
