5dfe892bfa2ce22bccb7a591d41373aec2672243
arrKhange1 <73338488+arrKhange1@users.noreply.github.com>
2021-06-29 22:33:31 +0500

    Update vk bot.py

--- vk bot.py
+++ vk bot.py
@@ -1,4 +1,5 @@
-from translate import Translator
+from google_trans_new import google_translator
+
 
 # working with images
 import imageio
@@ -12,26 +13,23 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import time
 import sys
 
-
 import random
-
 
 # collecting images into an array
 image_list = []
-for image_path in glob.glob("C:\\Users\\User\\Downloads\\Flags\\*.png"):
+for image_path in glob.glob("C:\\Users\\Артур\\Downloads\\Flags\\*.png"):
     im = imageio.imread(image_path)
     image_list.append(image_path)
-    print (image_path)
-    print (im.dtype)
-print(image_list[0])
+    #print(image_path)
+    #print(im.dtype)
+#print(image_list[0])
 
-# translater
-translator = Translator(from_lang="English", to_lang="Russian")
-
+# translator
+translator = google_translator()
 #
 
 token = 'dbfd8caa3a154800a5799603dce97730179d2069e91307b69bb5e616719e47ec808c53cb20d238d2127a4'
-vk_session = vk_api.VkApi(token= token)
+vk_session = vk_api.VkApi(token=token)
 session = vk_session.get_api()
 load = VkUpload(session)
 longpoll = VkLongPoll(vk_session)
@@ -39,8 +37,32 @@ s = ''
 timef = time.time()
 timemin = sys.maxsize
 
+def flag_name_precision(flag, resp): # >=40% mb
+    flag = flag.lower()
+    resp = resp.lower()
+    global_match = 0
+    match = 0
+    k = 0
+
+    if len(flag) < len(resp):
+        flag, resp = resp, flag
 
 
+    for i in range(len(flag)-len(resp)+1):
+        for j in range(i,i+len(resp)):
+            if flag[j] == resp[k]:
+                match += 1
+                print(match)
+            k += 1
+        if global_match < match:
+            global_match = match
+        match = 0
+        k = 0
+
+    precision = global_match / len(flag)
+    return precision
+
+print(flag_name_precision("Россия", "Расия"))
 
 def load_photo(upload, photo):
     response = upload.photo_messages(photo)[0]
@@ -51,18 +73,20 @@ def load_photo(upload, photo):
 
     return owner_id, photo_id, access_key
 
+
 def send_photo(session, peer_id, owner_id, photo_id, access_key):
     attachment = f'photo{owner_id}_{photo_id}_{access_key}'
     session.messages.send(
-        random_id = 0,
+        random_id=0,
         peer_id=peer_id,
         attachment=attachment
     )
 
+
 def create_keyboard(response):
     keyboard1 = VkKeyboard(one_time=True)
     if response == '/menu':
-        keyboard1.add_button('ИГРЫ',color=VkKeyboardColor.PRIMARY)
+        keyboard1.add_button('ИГРЫ', color=VkKeyboardColor.PRIMARY)
         keyboard1.add_button('КОМАНДЫ', color=VkKeyboardColor.PRIMARY)
     elif response == '[club159274465|@arrkhange1] игры' or response == '[club159274465|arrkhange1 - just a personality] игры':
         keyboard1.add_button('CASINO', color=VkKeyboardColor.NEGATIVE)
@@ -80,11 +104,11 @@ def create_keyboard(response):
         keyboard1.add_button('5', color=VkKeyboardColor.POSITIVE)
         keyboard1.add_button('6', color=VkKeyboardColor.NEGATIVE)
     elif response == '[club159274465|@arrkhange1] Далее':
-        keyboard1.add_button('Далее ->', color= VkKeyboardColor.POSITIVE)
+        keyboard1.add_button('Далее ->', color=VkKeyboardColor.POSITIVE)
 
 
     elif response == '/close':
-         return keyboard1.get_empty_keyboard()
+        return keyboard1.get_empty_keyboard()
     keyboard1 = keyboard1.get_keyboard()
     return keyboard1
 
@@ -101,63 +125,70 @@ for event in longpoll.listen():
         print(response)
         keyboard = create_keyboard(response)
 
-        #game_flags = True    maybe delete
-        #game_capcha = False
-
         # СООБЩЕНИЕ В ГРУППУ
-        if event.from_user and not event.from_me :
+        if event.from_user and not event.from_me:
 
             if response == '/commands':
-                session.messages.send(user_id= event.user_id, message = 'opening commands', random_id = 0, keyboard = keyboard)
+                session.messages.send(user_id=event.user_id, message='opening commands', random_id=0, keyboard=keyboard)
 
             elif response == '/ban':
-                vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'The user has been banned!', 'random_id': 0})
+                vk_session.method('messages.send',
+                                  {'user_id': event.user_id, 'message': 'The user has been banned!', 'random_id': 0})
 
             elif response == '/close':
                 vk_session.method('messages.send',
-                                                  {'user_id': event.user_id, 'message': 'closing a keyboard', 'random_id': 0, 'keyboard': keyboard})
+                                  {'user_id': event.user_id, 'message': 'closing a keyboard', 'random_id': 0,
+                                   'keyboard': keyboard})
 
 
         # СООБЩЕНИЕ ИЗ БЕСЕДЫ
         elif event.from_chat:
             n = 0
             if response == '[club159274465|@arrkhange1] команды':
-              session.messages.send(chat_id = event.chat_id, message = 'Список доступных команд: ', random_id = 0, keyboard = keyboard)
+                session.messages.send(chat_id=event.chat_id, message='Список доступных команд: ', random_id=0,
+                                      keyboard=keyboard)
 
-           # ВЫВОД УЧАСТНИКОВ БЕСЕДЫ
+            # ВЫВОД УЧАСТНИКОВ БЕСЕДЫ
 
             elif response == '[club159274465|@arrkhange1] участники':
-                chat = vk_session.method('messages.getConversationMembers', {'chat_id': event.chat_id, 'peer_id': event.peer_id})
+                chat = vk_session.method('messages.getConversationMembers',
+                                         {'chat_id': event.chat_id, 'peer_id': event.peer_id})
                 count = chat['count']
-                while n < count-1:
+                while n < count - 1:
                     member = chat['profiles'][n]['first_name']
                     member1 = chat['profiles'][n]['last_name']
-                    session.messages.send(chat_id = event.chat_id, message = member + ' ' + member1, random_id = 0)
+                    session.messages.send(chat_id=event.chat_id, message=member + ' ' + member1, random_id=0)
                     n += 1
                 n = 0
             #######################
             elif response == '/menu':
-                session.messages.send(chat_id = event.chat_id, message = 'Список моих возможностей: ', random_id = 0, keyboard= keyboard)
+                game_flags = False
+                session.messages.send(chat_id=event.chat_id, message='Список моих возможностей: ', random_id=0,
+                                      keyboard=keyboard)
 
             elif response == '[club159274465|@arrkhange1] игры' or response == '[club159274465|arrkhange1 - just a personality] игры':
                 session.messages.send(chat_id=event.chat_id, message='Список доступных игр с ботом: ', random_id=0,
                                       keyboard=keyboard)
 
             elif response == '/close':
-                session.messages.send(chat_id=event.chat_id, message= 'closing a keyboard', random_id=0, keyboard = keyboard)
+                session.messages.send(chat_id=event.chat_id, message='closing a keyboard', random_id=0,
+                                      keyboard=keyboard)
 
-        # CAPTCHA TRAINER
+            # CAPTCHA TRAINER
             elif response == 'rand' or response == '[club159274465|@arrkhange1] captcha trainer' or response == '[club159274465|arrkhange1 - just a personality] captcha trainer':
                 game_capcha = True
-                session.messages.send(chat_id=event.chat_id, message='Инструкция к игре "Капча-тренер": \n \n Поступает рандомная последовательность из латинских букв и цифр, Вы должны успеть напечатать ее за 4 секунды! \n \n Игра начнется через 10 секунд!', random_id=0)
+                session.messages.send(chat_id=event.chat_id,
+                                      message='Инструкция к игре "Капча-тренер": \n \n Поступает рандомная последовательность из латинских букв и цифр, Вы должны успеть напечатать ее за 4 секунды! \n \n Игра начнется через 10 секунд!',
+                                      random_id=0)
                 time.sleep(10)
-                #timef = time.time()
+                # timef = time.time()
                 p = 0
                 s = ''
 
                 b = []
-                a = ['a', 'b', 'c', 'd', 'e', 'f', '2', '5','1', '3','4', '6', '7', '8', '9', '0', 'g', 'h', 'i', 'j', 'i','k', 'l', 'm', 'n','o', 'p', 'r','s', 't', 'u', 'v', 'x', 'y', 'w', 'z']
-                while p < 5: # 6 symbols
+                a = ['a', 'b', 'c', 'd', 'e', 'f', '2', '5', '1', '3', '4', '6', '7', '8', '9', '0', 'g', 'h', 'i', 'j',
+                     'i', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z']
+                while p < 5:  # 6 symbols
                     b.append(random.choice(a))
                     p += 1
                 s = s.join(b)
@@ -167,26 +198,29 @@ for event in longpoll.listen():
                 game_capcha = False
                 timelast = time.time() - timef
                 if timelast <= 4:
-                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(timelast) + '\n \n Вы выиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
+                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(
+                        timelast) + '\n \n Вы выиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
                 else:
-                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(timelast) + '\n \n Вы проиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
+                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(
+                        timelast) + '\n \n Вы проиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
 
 
 
             elif response == 'хочу' or response == '+':
                 game_capcha = True
-                session.messages.send(chat_id=event.chat_id, message='Приготовься!! \n \n  Игра начнется через 5 секунд!', random_id=0)
+                session.messages.send(chat_id=event.chat_id,
+                                      message='Приготовься!! \n \n  Игра начнется через 5 секунд!', random_id=0)
                 time.sleep(5)
                 timef = time.time()
                 p = 0
                 s = ''
                 b = []
-                a = ['a', 'b', 'c', 'd', 'e', 'f', '2', '5','1', '3','4', '6', '7', '8', '9', '0', 'g', 'h', 'i', 'j', 'i', 'k', 'l', 'm', 'n','o', 'p', 'r','s', 't', 'u', 'v', 'x', 'y', 'w', 'z']
+                a = ['a', 'b', 'c', 'd', 'e', 'f', '2', '5', '1', '3', '4', '6', '7', '8', '9', '0', 'g', 'h', 'i', 'j',
+                     'i', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'x', 'y', 'w', 'z']
                 while p < 6:
                     b.append(random.choice(a))
                     p += 1
                 s = s.join(b)
-
 
                 session.messages.send(chat_id=event.chat_id, message=s + '\n \n Время пошло!', random_id=0)
                 timef = time.time()
@@ -194,9 +228,11 @@ for event in longpoll.listen():
                 game_capcha = False
                 timelast = time.time() - timef
                 if timelast <= 4:
-                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(timelast) + ' \n \n Вы выиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
+                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(
+                        timelast) + ' \n \n Вы выиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
                 else:
-                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(timelast) + '\n \n  Вы проиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
+                    session.messages.send(chat_id=event.chat_id, message='Ваше время: ' + str(
+                        timelast) + '\n \n  Вы проиграли! \n \n Хочешь еще? Пиши "хочу" или "+"!', random_id=0)
 
 
 
@@ -205,24 +241,27 @@ for event in longpoll.listen():
 
 
 
-         # CASINO
+            # CASINO
             elif response == '[club159274465|@arrkhange1] casino' or response == '[club159274465|arrkhange1 - just a personality] casino':
-                session.messages.send(chat_id=event.chat_id, message='Инструкция к игре "Казино": \n \n Вы кидаете кубик. Если Ваше число совпало с числом бота - Вы выиграли! \n \n Перестать играть - /close \n \n Через 10 секунд игра начнется!', random_id=0)
+                session.messages.send(chat_id=event.chat_id,
+                                      message='Инструкция к игре "Казино": \n \n Вы кидаете кубик. Если Ваше число совпало с числом бота - Вы выиграли! \n \n Перестать играть - /close \n \n Через 10 секунд игра начнется!',
+                                      random_id=0)
 
                 time.sleep(10)
 
-                session.messages.send(chat_id=event.chat_id, message='Кидай кубик!', random_id=0, keyboard = keyboard)
+                session.messages.send(chat_id=event.chat_id, message='Кидай кубик!', random_id=0, keyboard=keyboard)
 
 
 
 
             elif response == '[club159274465|@arrkhange1] 1' or response == '[club159274465|@arrkhange1] 2' or response == '[club159274465|@arrkhange1] 3' or response == '[club159274465|@arrkhange1] 4' or response == '[club159274465|@arrkhange1] 5' or response == '[club159274465|@arrkhange1] 6':
-                botnum = random.randint(1,6)
+                botnum = random.randint(1, 6)
                 session.messages.send(chat_id=event.chat_id, message='Мое число: ' + str(botnum), random_id=0)
                 print(botnum)
                 if '[club159274465|@arrkhange1] ' + str(botnum) == response:
                     keyboard = VkKeyboard.get_empty_keyboard()
-                    session.messages.send(chat_id=event.chat_id, message='WIN!' + '\n \n Продолжим?', random_id=0, keyboard = keyboard)
+                    session.messages.send(chat_id=event.chat_id, message='WIN!' + '\n \n Продолжим?', random_id=0,
+                                          keyboard=keyboard)
 
             elif response == 'да' or response == 'lf' or response == 'da':
                 da = create_keyboard('[club159274465|@arrkhange1] casino')
@@ -235,23 +274,32 @@ for event in longpoll.listen():
                 game_flags = True
                 session.messages.send(chat_id=event.chat_id, message="Назовите эту страну:\n\n", random_id=0)
                 rand_gen = random.randint(0, 259)  # instead of zeros below
-                send_photo(session, event.peer_id, *load_photo(load, image_list[0]))
-                flag_name = image_list[0][::-1][:image_list[0][::-1].find('\\')][4:][::-1]
+                send_photo(session, event.peer_id, *load_photo(load, image_list[rand_gen]))
+                flag_name = image_list[rand_gen][::-1][:image_list[rand_gen][::-1].find('\\')][4:][::-1]
+                flag_name = flag_name.replace('-', ' ')
+                print(list(flag_name))
 
-            elif response == flag_name.lower() and game_flags:
-                print(11111)
+            elif flag_name_precision(response,translator.translate(flag_name.lower(), lang_src='en', lang_tgt='ru').lower().strip() ) >= 0.4 and game_flags:
+
+                cntry_name = translator.translate(flag_name, lang_src='en', lang_tgt='ru').strip()
                 flags_named += 1
-                session.messages.send(chat_id=event.chat_id, message="Правильно!\nФлагов названо: " + str(flags_named) +" / 5\nДо победы осталось назвать флагов: " + str(5-flags_named)+"\n\n", random_id=0, keyboard = create_keyboard('[club159274465|@arrkhange1] Далее'))
+                session.messages.send(chat_id=event.chat_id, message="Правильно! Страна - " + cntry_name + "!\nФлагов названо: " + str(
+                    flags_named) + " / 5\nДо победы осталось назвать флагов: " + str(5 - flags_named) + "\n\n",
+                                      random_id=0, keyboard=create_keyboard('[club159274465|@arrkhange1] Далее'))
                 if flags_named == 5:
                     session.messages.send(chat_id=event.chat_id, message="Поздравляю, Вы победили!\n\n", random_id=0)
                     game_flags = False
 
             elif game_flags and not event.from_me:
-                session.messages.send(chat_id=event.chat_id, message="Вы проиграли!\nФлагов названо: " + str(flags_named) + " / 5\n До победы не хватило флагов: " + str(5-flags_named)+"\n\n", random_id=0)
+
+                cntry_name = translator.translate(flag_name, lang_src='en', lang_tgt='ru').strip()
+                print(list(cntry_name))
+                session.messages.send(chat_id=event.chat_id, message="Вы проиграли! На самом деле это " + cntry_name + "!\nФлагов названо: " + str(
+                    flags_named) + " / 5\n До победы не хватило флагов: " + str(5 - flags_named) + "\n\n", random_id=0)
                 flags_named = 0
                 game_flags = False
 
-        
+
 
 
 
